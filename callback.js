3f06e22becd0de2e57eb2f355dd27f57f7869065
John Jorgensen <john@jorgensenfamily.us>
2023-05-04 16:59:01 -0700

    create getToken function

--- callback.js
+++ callback.js
@@ -1,19 +1,26 @@
-const authCode = new URLSearchParams(window.location.search).get("code");
+const authURL = "https://message-journal.auth.us-west-2.amazoncognito.com";
+const redirectURI = window.location.origin + "/callback.html";
+const clientID = "2ji6bjoqm4p37s1r87t1099n0a";
 
-fetch(authURL + "/oauth2/token", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: new URLSearchParams({
-        grant_type: "authorization_code",
-        client_id: clientID,
-        redirect_uri: redirectURI,
-        code: authCode
+export function getTokens() {
+    const authCode = new URLSearchParams(window.location.search).get("code");
+    console.log("get");
+
+    return fetch(authURL + "/oauth2/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            grant_type: "authorization_code",
+            client_id: clientID,
+            redirect_uri: redirectURI,
+            code: authCode
+        })
     })
-})
-.then(response => response.json())
-.then(data => writeTokens(data));
+    .then(response => response.json())
+    .then(data => writeTokens(data));
+}
 
 function writeTokens(data) {
     window.localStorage.setItem("id_token", data.id_token);
