b66bac73e58d52cde013767a43fa5a6852b9964c
John Jorgensen <john@jorgensenfamily.us>
2023-05-13 17:07:57 -0700

    add credential fetching (need to fix scope issue)

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./test.js",
+    output: {
+        filename: "./bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: "development"
+}
