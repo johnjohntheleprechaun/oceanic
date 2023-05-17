498e884ddcce180f28ba981d1234eb321a792ebe
John Jorgensen <john@jorgensenfamily.us>
2023-05-17 05:42:04 -0700

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./test.js",
     output: {
         filename: "./bundle.js",
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'js')
     },
     mode: "development"
 }
