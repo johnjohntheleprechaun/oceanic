c7a23931bb6e058f86a7749b0df60931557cb39f
John Jorgensen <john@jorgensenfamily.us>
2023-05-25 13:04:12 -0700

    move JS files

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ var path = require('path');
 
 module.exports = {
     entry: {
-        test: "./test.js",
-        home: "./home.js",
-        journal: "./journal.js"
+        test: "./src/test.js",
+        home: "./src/home.js",
+        journal: "./src/journal.js"
     },
     output: {
         filename: "[name].bundle.js",
