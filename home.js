fe6ea557535be3508a0fd7ddd79ee485d68d30ad
John Jorgensen <john@jorgensenfamily.us>
2023-05-25 10:27:45 -0700

    switch journal list to AWS utils

--- home.js
+++ home.js
@@ -1,9 +1,11 @@
+import { dynamoScan, utilsInit } from "./aws";
+
 const url = "https://4bwtjf5ctmo527paml7vdxnwnq0yhiuf.lambda-url.us-west-2.on.aws/journals/";
 
 let entryTemplate;
 let journalArea;
 
-window.addEventListener("load", () => {token = loadToken()});
+window.addEventListener("load", utilsInit);
 window.addEventListener("load", loadJournals);
 window.addEventListener("load", () => {
     entryTemplate = document.getElementById("journal-entry-template").content.querySelector(".journal-entry ");
@@ -24,31 +26,27 @@ function createJournal(event) {
     .catch(error => console.error(error));
 }
 
-function loadJournals() {
-    fetch(url, {
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer " + token
-        }
-    })
-    .then(response => response.json())
-    .then(setJournals)
-    .catch(error => console.error(error));
+async function loadJournals() {
+    const params = {
+        TableName: "journal-entry-list"
+    };
+    const journals = await dynamoScan(params);
+    setJournals(journals);
 }
 
 function setJournals(data) {
-    let sorted = data.Items.sort((a,b) => a.created >= b.created ? 1 : -1);
+    const sorted = data.sort((a,b) => a.created.N >= b.created.N ? 1 : -1);
     sorted.forEach(addJournal);
 }
 
 function addJournal(journal) {
     let entry = entryTemplate.cloneNode(true);
-    let date = new Date(journal.created);
+    let date = new Date(parseInt(journal.created.N));
 
     entry.querySelector(".date").innerText = (date.getMonth() + 1).toString().padStart(2,"0") + "/" + date.getDate().toString().padStart(2,"0") + "/" + date.getFullYear();
     entry.querySelector(".time").innerText = date.getHours().toString().padStart(2,"0") + ":" + date.getMinutes().toString().padStart(2,"0");
     entry.addEventListener("click", event => openJournal(event.target.attributes["data-entryid"].nodeValue));
-    entry.dataset.entryid = journal.entryID;
+    entry.dataset.entryid = journal.entryID.S;
 
     journalArea.appendChild(entry);
 }
