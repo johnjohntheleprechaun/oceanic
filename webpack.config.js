92da3ac4c2719bf6a6270454b3b605f0d0cf94c0
John Jorgensen <john@jorgensenfamily.us>
2023-06-19 16:59:15 -0700

    typescript config

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,25 @@ var path = require('path');
 
 module.exports = {
     entry: {
-        test: "./src/test.js",
+        test: "./src/test.ts",
         home: "./src/home.js",
         journal: "./src/journal.js"
     },
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, 'js')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts"]
     },
     mode: "development"
 }
