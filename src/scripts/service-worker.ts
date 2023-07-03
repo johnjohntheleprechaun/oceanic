4022ccc46259ccb6f3b828b514e14f93fb7e263b
John Jorgensen <john@jorgensenfamily.us>
2023-07-03 18:14:45 -0400

    move caching before old cache deletion. idk why

--- src/scripts/service-worker.ts
+++ src/scripts/service-worker.ts
@@ -1,6 +1,6 @@
 // cache version 0 will be used for testing only
 // otherwise cache versions should match application versions.
-const CACHE_VERSION = 0;
+const CACHE_VERSION = 1;
 const CACHE_FILES = [
     "/home.html", "/home.js", "home.css", "/icon_add.svg", // journal list
     "/journal.html", "/journal.js", "journal.css", "/icon_send.svg", // journal editor
@@ -11,24 +11,27 @@ let cache: Cache;
 addEventListener("fetch", fetchEvent);
 addEventListener("activate", activateWorker);
 async function activateWorker(event: ExtendableEvent) {
+    console.log("activating...");
     cache = await loadCache(CACHE_VERSION);
+    console.log("activated");
 }
 
 async function loadCache(version: number): Promise<Cache> {
     const cacheName = `oceanicV${version}`;
+
+    // cache all required files if there's a new version (or if test)
+    if (!(cacheName in caches.keys()) || version === 0) {
+        let cache = await caches.open(cacheName);
+        for (let file of CACHE_FILES) {
+            await cache.add(file);
+        }
+    }
+
     // delete all caches that aren't of the current version
     const cacheList = await caches.keys();
     for (let cache of cacheList) {
         if (cache !== cacheName) {
             caches.delete(cache);
-        }
-    }
-
-    // cache all required files if there's a new version (or if test)
-    let cache = await caches.open(cacheName);
-    if (cacheList.length === 0 || version === 0) {
-        for (let file of CACHE_FILES) {
-            await cache.add(file);
         }
     }
 
