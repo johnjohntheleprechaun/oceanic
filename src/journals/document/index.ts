6c9a8c6978a2d7b58a91be4bb0ade07be14400a6
John Jorgensen <john@jorgensenfamily.us>
2023-11-03 01:46:31 -0700

    add new page adding

--- src/journals/document/index.ts
+++ src/journals/document/index.ts
@@ -2,21 +2,40 @@ import { Journal, appendToJournal, dbInit, getJournal } from "../../scripts/util
 
 let entryID: string;
 let journal: Journal;
-let pages: HTMLCollectionOf<Element>;
+let pages: HTMLDivElement[];
+let editor: HTMLDivElement;
 
 window.addEventListener("load", async () => {
     await dbInit();
     entryID = parseHash().entryid;
     journal = await getJournal(entryID);
     setTitle(journal.created);
-    pages = document.getElementsByClassName("page");
+    editor = document.getElementById("editor") as HTMLDivElement;
+    pages = Array.from(editor.children) as HTMLDivElement[];
 
     setPageSizes();
+    updateContent();
 });
 window.addEventListener("resize", function() {
     setPageSizes();
 });
 
+function updateContent() {
+    const content = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisis leo vel fringilla est. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Tempus egestas sed sed risus pretium quam. Dolor sit amet consectetur adipiscing elit ut. Id porta nibh venenatis cras sed felis. Ornare arcu dui vivamus arcu felis bibendum ut tristique. Fermentum odio eu feugiat pretium nibh ipsum. Et pharetra pharetra massa massa. Magna eget est lorem ipsum dolor sit. Tortor condimentum lacinia quis vel eros.\n\n`.repeat(100);
+    const paragraphs = content.split(/\n+/);
+    for (const paragraph of paragraphs) {
+        const page = pages[pages.length-1].children.item(0) as HTMLDivElement;
+        const prevText = page.innerText
+        page.innerText += paragraph;
+        if (page.scrollHeight > page.clientHeight) {
+            page.innerText = prevText;
+            const newPage = pages[pages.length-1].cloneNode(true) as HTMLDivElement;
+            (newPage.children.item(0) as HTMLDivElement).innerText = paragraph;
+            editor.appendChild(newPage);
+            pages.push(newPage);
+        }
+    }
+}
 
 function setPageSizes() {
     let page = document.getElementsByClassName("page").item(0) as HTMLDivElement;
