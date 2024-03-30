493739d78a159e2b8991f31606e347223a86f3e8
John Jorgensen <john@jorgensenfamily.us>
2024-03-30 04:33:12 -0700

    update JSON schemas for user settings

--- src/scripts/utils/settings.ts
+++ src/scripts/utils/settings.ts
@@ -1,18 +1,44 @@
 import { FromSchema, JSONSchema } from "json-schema-to-ts";
 
+// All settings schemas should have local and global. Global settings are per-account, local are device-specific
+const settingsGroupTemplate = {
+    type: "object",
+    properties: {
+        "local": {
+            type: "object"
+        },
+        "global": {
+            type: "object"
+        }
+    },
+    required: [ "global" ]
+} as const satisfies JSONSchema;
+
 const securitySettingsSchema = {
     type: "object",
     properties: {
-        "deviceTrustLevel": {
-            enum: [ "none", "minimal", "secure" ],
-            title: "Device Trust Level",
-            description: "How much this device should be trusted. This affects where the unwrapped master key is stored, or if it's stored at all.",
-            default: "none"
+        "local": {
+            type: "object",
+            properties: {
+                "deviceTrustLevel": {
+                    enum: [ "none", "minimal", "secure" ],
+                    title: "Device Trust Level",
+                    description: "How much this device should be trusted. This affects where the unwrapped master key is stored, or if it's stored at all.",
+                    default: "none"
+                }
+            },
+            required: [ "deviceTrustLevel" ]
+        },
+        "global": {
+            type: "object",
+            properties: {
+
+            }
         }
     },
-    required: [ "deviceTrustLevel" ]
+    required: [ "global" ]
 } as const satisfies JSONSchema;
-type SecuritySettings = FromSchema<typeof securitySettingsSchema>;
+// type SecuritySettings = FromSchema<typeof securitySettingsSchema>;
 
 const userSettingsSchema = {
     type: "object",
@@ -23,6 +49,6 @@ const userSettingsSchema = {
             oneOf: [ securitySettingsSchema ]
         }
     },
-    required: [ "user", "securitySettings" ]
+    required: [ "securitySettings" ]
 } as const satisfies JSONSchema;
 type UserSettings = FromSchema<typeof userSettingsSchema>;
